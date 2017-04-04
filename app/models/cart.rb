c8480ed993aa054db2543e9d096351cbc407c59c
Luke Ghenco <lukeghenco@gmail.com>
2017-04-04 11:37:13 -0400

    add passing code for Cart's set_quantity instance method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,14 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
   has_many :items, through: :line_items
+
+  def set_quantity(line_item = nil, item, quantity)
+    if line_item 
+      quantity_to_update = line_item.quantity + quantity
+      item.is_inventory_available?(quantity_to_update) ? quantity_to_update : item.inventory
+    else 
+      item.is_inventory_available?(quantity) ? quantity : item.inventory 
+    end
+  end
+      
 end
