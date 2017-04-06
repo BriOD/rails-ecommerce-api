8e0086504ff8c6fc2e52e08003e8f84ff7410861
Luke Ghenco <lukeghenco@gmail.com>
2017-04-06 10:06:18 -0400

    add passing POST /api/v1/users test

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do 
+RSpec.describe "Api::Users", type: :request do 
 
   describe "POST /users" do 
 
     describe "on success" do 
       
-      it "creates a user from the params"
+      it "creates a user from the params" do
+        params = { 
+          user: { 
+            email: 'luke@flatironschool.com', 
+            password: 'password' 
+          } 
+        }
+
+        post "/api/v1/users", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq('luke@flatironschool.com')
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)
+      end
 
       it "returns the new user and a JWT token"
     end
